807c533f20fb45ced8a2bcde8acf05e239ac5a62
Peck-man <simonpeck97@gmail.com>
2022-08-30 09:38:35 +0200

    connect to db and model of student

--- src/main/java/com/example/coursemanagementsystem/CourseManagementSystemApplication.java
+++ src/main/java/com/example/coursemanagementsystem/CourseManagementSystemApplication.java
@@ -1,13 +1,17 @@
 package com.example.coursemanagementsystem;
 
+import com.example.coursemanagementsystem.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseManagementSystemApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(CourseManagementSystemApplication.class, args);
+
     }
 
 }
