76add0f24977110f0461d7a38be0d28860901e1e
Peck-man <simonpeck97@gmail.com>
2022-08-30 10:36:01 +0200

    add model for administrator

---
+++ src/main/java/com/example/coursemanagementsystem/models/Administrator.java
@@ -0,0 +1,30 @@
+package com.example.coursemanagementsystem.models;
+
+
+public class Administrator extends Person{
+
+
+    public Administrator(String name, String password){
+        super(name, password);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+}
